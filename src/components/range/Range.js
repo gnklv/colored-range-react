9cba3e68e44b1bc09ff169f696c1326d775b37d0
Georgy Nikolaev <wgg357@gmail.com>
2020-03-11 02:26:32 +0500

    Finishing

--- src/components/range/Range.js
+++ src/components/range/Range.js
@@ -29,8 +29,9 @@ const useStyles = ({ gradient, thumbs, colors }) => {
     }
 };
 
-const useListeners = ({ thumbs, setThumbs, minSizeNear, min, max, refRange }) => {
+const useListeners = ({ thumbs, setThumbs, min, max, refRange }) => {
     let activeI = 0;
+    const minSizeNear = 5;
 
     const addEventListenerOnce = (eventName, cb) => {
         const once = (event) => {
@@ -106,6 +107,7 @@ const useColors = ({ thumbs, gradient, thumbSize }) => {
     };
 
     const calculateColor = (colorValue) => {
+        // colorValue: 75
         const colorRange = {};
         for (let i = 0; i < gradient.length; i += 1) {
             const { value } = gradient[i];
@@ -115,34 +117,46 @@ const useColors = ({ thumbs, gradient, thumbSize }) => {
                 break;
             }
         }
+        // colorRange.firstI: 1
+        // colorRange.secondI: 2
 
         const firstColor = gradient[colorRange.firstI].color;
         const secondColor = gradient[colorRange.secondI].color;
+        // firstColor: [255, 211, 6]
+        // secondColor: [0, 130, 9]
 
         const firstColorX = thumbSize * (gradient[colorRange.firstI].value / 100);
+        // firstColorX: 18 * (50 / 100) = 9
         const secondColorX = (thumbSize * (gradient[colorRange.secondI].value / 100))
             - firstColorX;
+        // secondColorX: (18 * (100 / 100) - 9) = 9
         const sliderX = thumbSize * (colorValue / 100) - firstColorX;
+        // sliderX: 18 * (75 / 100) - 9 = 4.5
         const ratio = sliderX / secondColorX;
+        // ratio: 4.5 / 9 = 0.5
 
-        return `rgb(${pickHex(secondColor, firstColor, ratio).join(',')})`;
+        return `rgb(${pickColor(firstColor, secondColor, ratio).join(',')})`;
     };
 
-    const pickHex = (color1, color2, weight) => {
-        const w = weight * 2 - 1;
-        const w1 = (w + 1) / 2;
-        const w2 = 1 - w1;
+    const pickColor = (color1, color2, ratio) => {
+        // color1: [255, 211, 6]
+        // color2: [0, 130, 9]
+        // ration: 0.5
+        const r = ratio * 2 - 1;
+        // r: 0.5 * 2 - 1 = 0
+        const r1 = (r + 1) / 2;
+        // r1 = (0 + 1) / 2 = 0.5
+        const r2 = 1 - r1;
+        // r2 = (1 - 0.5) = 0.5
 
         return [
-            Math.round(color1[0] * w1 + color2[0] * w2),
-            Math.round(color1[1] * w1 + color2[1] * w2),
-            Math.round(color1[2] * w1 + color2[2] * w2),
+            Math.round(color2[0] * r1 + color1[0] * r2), // 255 * 0.5 + 0 * 0.5 = 128 (red)
+            Math.round(color2[1] * r1 + color1[1] * r2), // 211 * 0.5 + 130 * 0.5 = 171 (green)
+            Math.round(color2[2] * r1 + color1[2] * r2), // 6 * 0.5 + 9 * 0.5 = 8 (blue)
         ];
     };
 
-    useEffect(() => {
-        initColors();
-    }, [thumbs]);
+    useEffect(initColors, [thumbs]);
 
     return { colors };
 };
@@ -152,7 +166,6 @@ const Range = () => {
     const max = 100;
 
     const thumbSize = 18;
-    const minSizeNear = 5;
     const gradient = [
         {
             value: 0,
@@ -168,12 +181,12 @@ const Range = () => {
         }
     ];
 
-    const [thumbs, setThumbs] = useState([0, 100]);
+    const [thumbs, setThumbs] = useState([75, 100]);
     const refRange = useRef(null);
 
     const { colors } = useColors({ gradient, thumbs, thumbSize });
     const { rangeStyles, thumbStyles, thumbValueStyles } = useStyles({ gradient, thumbs, colors });
-    const { onThumbMouseDown } = useListeners({ thumbs, setThumbs, minSizeNear, min, max, refRange });
+    const { onThumbMouseDown } = useListeners({ thumbs, setThumbs, min, max, refRange });
 
     return (
         <div className='wrapper'>
